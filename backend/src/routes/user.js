6370ddb7171ad2ae02b2673dc4d48fa891316035
Vuxpn <vuphannguyen15102003@gmail.com>
2024-04-05 21:59:07 +0700

    reset

--- backend/src/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const userController = require('../app/controllers/UserController');
-
-/* router.get('/:slug', userController.show);
-router.get('/', userController.index); */
-
-module.exports = router;
