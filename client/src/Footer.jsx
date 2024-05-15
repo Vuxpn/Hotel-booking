7fda8b81db6998156f1ac7f93f4885b8bacff863
Vuxpn <vuphannguyen15102003@gmail.com>
2024-05-15 23:52:38 +0700

    update footer

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -2,18 +2,18 @@ export default function Footer() {
     return (
         <div className="mt-auto pt-10 px-14 pb-6 bg-gray-100 flex justify-between">
             <div>
-                <h1 className="font-bold text-xl pb-2">StayEasy</h1>
+                <h1 className="font-bold text-xl pb-2">TravelBooking</h1>
                 <h1 className="text-gray-500">
-                    Your favorite hotels booking <br />
-                    experience since 2023!
+                    Khám phá thế giới dễ dàng <br />
+                    Đặt ngay trên TravelBooking
                 </h1>
             </div>
             <div className="text-gray-500 text-right">
-                <h1 className="font-semibold text-black">Contact us</h1>
-                <h1>Group 38</h1>
+                <h1 className="font-semibold text-black">Nhóm 38</h1>
+                <h1></h1>
                 <h1>
-                    Web Technology and <br />
-                    E-services
+                    Công nghệ Web <br />
+                    và dịch vụ trực tuyến
                 </h1>
             </div>
         </div>
